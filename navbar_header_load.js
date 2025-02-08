a8c3e3c6db683e6d4ca72ba7634fec96dd90e399
Conway Zhou <ideaczhou@gmail.com>
2025-02-08 00:32:03 -0500

    reverting

--- navbar_header_load.js
+++ navbar_header_load.js
@@ -1,8 +1,4 @@
-function loadNavbarHeader(){
-    document.getElementById("header").innerHTML = 
-    <div style="text-align:center; background-image:linear-gradient(#32cd32, #8eda81); padding:50px; position:sticky">
-        <h1>Conway Zhou</h1>
-    </div>;
-    ("#nav-placeholder").load("navbar.html");
-    ("#header-placeholder").load("header.html");
-  }
+$(function(){
+    $("#nav-placeholder").load("html_templates/navbar.html");
+    $("#header-placeholder").load("html_templates/header.html");
+  });

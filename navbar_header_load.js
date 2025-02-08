3bee96893bba5623973290452781954b56d8239f
Conway Zhou <ideaczhou@gmail.com>
2025-02-08 00:03:41 -0500

    testing

--- navbar_header_load.js
+++ navbar_header_load.js
@@ -1,4 +1,8 @@
 function loadNavbarHeader(){
-    $("#nav-placeholder").load("navbar.html");
-    $("#header-placeholder").load("header.html");
+    document.getElementById("header").innerHTML = 
+    <div style="text-align:center; background-image:linear-gradient(#32cd32, #8eda81); padding:50px; position:sticky">
+        <h1>Conway Zhou</h1>
+    </div>;
+    ("#nav-placeholder").load("navbar.html");
+    ("#header-placeholder").load("header.html");
   }
